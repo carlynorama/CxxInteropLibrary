c30f137e6215991602c5bb6df3a3e703facd73e8
carlynorama <carlynorama@gmail.com>
2023-08-14 12:09:55 -0700

    start std type canary

--- Sources/InteropLibrary/ThingCatalog.swift
+++ Sources/InteropLibrary/ThingCatalog.swift
@@ -27,7 +27,29 @@ public struct ThingCatalog {
     //MARK: Misc C++ Function Tests
     public func sendAndReceiveString(send:String) -> String {
         let received = echo_this(std.string(send))
+        //TODO: Make an official std types canary.
+        
         return String(received)
+    }
+    
+    //TODO: Non string C++ inits from inside XCode.
+    //TODO: Find where can find current state o' affairs
+    public func stdTypesCanary() {
+        //re-added to keep an eye on:
+        let x = std.string("Howdy")
+        
+        //'std.__1.array<_Tp, _Size>' cannot be constructed because it has no accessible initializers
+        //Generic parameter '_Tp' could not be inferred
+        //Command SwiftCompile failed with a nonzero exit code
+        //Below Set: Doesn't complain in XCode, borks compiler.
+        //let test = std.array<Any, Any>(Array([2, 3, 4]))
+        //let y = std.array<UInt8, UInt8>(3, [8, 4, 5])
+        
+        //cant make template.
+        //let y = std.vector<CInt, CInt>(3, [8, 4, 5])
+        
+        //Expected 3, could not make template.
+        //let z = std.set<Int32, Int32, Int32>(Set(3,2,1))
     }
 
     public func currentNumber() -> Int {
